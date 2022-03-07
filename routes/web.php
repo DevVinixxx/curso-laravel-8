9af6a5da1a24e8995f77f963b9e876fc037d647b
Vinicius Peres <vperes505@gmail.com>
2022-03-07 16:19:08 -0300

    registros em laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/post',[ PostController::class,'index']);
+
 
 Route::get('/', function () {
     return view('welcome');
