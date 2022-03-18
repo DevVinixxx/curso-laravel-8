06eab13a3ee5dd05f4c1f7cc662a0ba390c3785b
Vinicius Peres <vperes505@gmail.com>
2022-03-18 15:18:20 -0300

    vinicin

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,18 +2,23 @@
 @section('title','Welcome')
 @section('content')
 <div class="p-2">
-    <h1>Detalhes do post:<br> {{ $post->title }}</h1>
+    <div class="">
+        <h1>Detalhes do post:<br> {{ $post->title }}</h1>
 
-    <ul>
-        <li><strong> Titutlo: </strong>{{ $post->title }}</li>
-        <li><strong> Conteudo: </strong>{{ $post->content }}</li>
-    </ul>
+        <ul>
+            <li><strong> Titutlo: </strong>{{ $post->title }}</li>
+            <li><strong> Conteudo: </strong>{{ $post->content }}</li>
+        </ul>
 
-    <form action="{{ route('posts.destroy', $post->id) }}" method="post">
-        @csrf
-        <input type="hidden" name="_method" value="DELETE" id="">
-        <button type="submit">Deletar post {{ $post->title }}</button>
-    </form>
-    <a href="{{ route('post.create') }}">Criarrrr</a>
+        <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+            @csrf
+            <input type="hidden" name="_method" value="DELETE" id="">
+            <button type="submit">Deletar post {{ $post->title }}</button>
+        </form>
+        <a href="{{ route('post.create') }}">Criarrrr</a>
+        <div class="button">
+            <a class="btn btn-primary" href="{{ route('post.index') }}" role="button">Voltar</a>
+        </div>
+    </div>
 </div>
 @endsection
