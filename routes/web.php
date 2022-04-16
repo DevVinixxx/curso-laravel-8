10e537a38cf6af196d14c4af458afff5852ebfbe
Vinicius Peres <vperes505@gmail.com>
2022-04-15 22:37:14 -0300

    conclusao do curso Laravel 8

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/post/search', [PostController::class, 'search'])->name('posts.search');
+Route::middleware(['auth'])->group(function () {
+    Route::any('/post/search', [PostController::class, 'search'])->name('posts.search');
 
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
 
-Route::put('/post/{id}/', [PostController::class, 'update'])->name('posts.update');
+    Route::put('/post/{id}/', [PostController::class, 'update'])->name('posts.update');
 
-Route::get('post/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::get('post/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
 
-Route::delete('/post/{id}/delete', [PostController::class,'destroy'])->name('posts.destroy');
+    Route::delete('/post/{id}/delete', [PostController::class,'destroy'])->name('posts.destroy');
 
-Route::get('/post/{id}/show',[PostController::class,'show'])->name('posts.show');
+    Route::get('/post/{id}/show',[PostController::class,'show'])->name('posts.show');
 
-Route::post('/post', [PostController::class, 'store'])->name('posts.store');
+    Route::post('/post', [PostController::class, 'store'])->name('posts.store');
 
-Route::get('/post',[ PostController::class,'index'])->name('post.index');
+    Route::get('/post',[ PostController::class,'index'])->name('post.index');
+});
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
